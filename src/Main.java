667fa28f768814c795a776cce29b1a5126dd6a19
pzjl00 <pzjl00@student.cis.edu.hk>
2023-03-06 10:42:02 +0800

    forgot to do this yesterday

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
         //objects
         Scanner input = new Scanner(System.in);
         //Game variables
-        String zombie = "zombie";
         int maxZombieHealth = 100;
         //player information
         System.out.println("How old are you?");
@@ -22,8 +21,8 @@ public class Main {
         System.out.println("Welcome to the zombie apocalypse!");
         GAME:
         while (playing) {
-            int zombieHealth = (int) (maxZombieHealth * (userAge % 10));
-            System.out.println(zombie + "appeared in your classroom! \n");
+            int zombieHealth = (int) (maxZombieHealth * (userAge/100));
+            System.out.println("A zombie appeared in your classroom! \n");
             while (zombieHealth > 0) {
                 playerStats(health, numArrows, numMedicine);
                 System.out.println("What would you like to do? If you would like to view your personal statistics later on in the game, enter the letter s.");
@@ -36,14 +35,17 @@ public class Main {
                     continue GAME;
                 }
                 else if (action.contains("attack")) {
-                    int damageCaused = (int)userAge + 5;
+                    int damageCaused = (int)damageCaused(userAge);
                     int damageTaken = (int)userAge - 5;
 
-                    zombieHealth -= damageCaused;
-                    health -= damageTaken;
+                    zombieHealth = zombieHealth(zombieHealth, damageCaused);
+                    health = health(health, damageTaken);
+                    numArrows = numArrows(numArrows);
 
-                    System.out.println("> You hit the zombie with an arrow.");
+                    System.out.println("You hit the zombie with an arrow.");
                     System.out.println("Damage received = " + damageTaken);
+                    System.out.println("Damage caused = " + damageCaused);
+                    System.out.println("Current zombie health = " + zombieHealth);
 
                     if (health <= 0) {
                         System.out.println("Unfortunately, the zombie has fatally injured your before you could kill it.");
@@ -52,17 +54,41 @@ public class Main {
                 }
                 else if ((action.equals("drink healing potion")) || (action.equals("drink potion"))) {
                     if(numMedicine > 0) {
-                        System.out.println(medicineEffect(health, medicineHeal));
+                        health = (medicineEffect(health, medicineHeal));
                         numMedicine --;
-                        System.out.println("You drink a healing potion. Almost immediately, your wounds start to heal and colour returns to your face. You feel more energized.\n You now have " + health + "health.\n" + "You have " + numMedicine + "healing potions.");
+                        System.out.println("You drink a healing potion. Almost immediately, your wounds start to heal and colour returns to your face. You feel more energized.\nYou now have " + health + " health.\n" + "You now have " + numMedicine + " healing potions.");
                     }
                     else {
                         System.out.println("You have no healing potions left! Kill zombies for a chance to collect them!");
                     }
                 }
                 else {
-
+                    System.out.println("Sorry, I don't understand what you're saying.");
                 }
+            }
+            if (health < 1) {
+                System.out.println("You are barely able to stay conscious as you stumble towards the door, unable to fight for any longer. \nGAME OVER");
+                break;
+            }
+            System.out.println("The zombie has been defeated. You win!");
+            System.out.println("Your remaining health: " + health);
+            if (chanceMedicine >= 20) {
+                medicine(numMedicine);
+                System.out.println("You now have" + numMedicine + "healing potion(s)");
+            }
+            System.out.println("What would you like to do?");
+            String choice = input.nextLine();
+            while (!choice.contains("fight") && !choice.contains("kill") && !choice.contains("leave") && !choice.contains("exit")) {
+                System.out.println("Sorry, I don't understand what you're saying.");
+                choice = input.nextLine();
+            }
+
+            if (choice.contains("fight") || choice.contains("kill")) {
+                System.out.println("You eagerly go to the corridor to find more zombies.");
+            }
+            else if(choice.contains("leave") || choice.contains("exit")) {
+                System.out.println("You exit the school victorious from combat.");
+                break;
             }
         }
     }
@@ -70,9 +96,25 @@ public class Main {
         return (age/2);
     }
     public static String playerStats (int health, int numArrows, int numMedicine) {
-        return "Your health: " + health + "\nYou have " + numMedicine + "healing potions" + "Your weapon is a bow. You currently have " + numArrows + "arrows.";
+        return "Your health: " + health + "\nYou have " + numMedicine + " healing potions." + " Your weapon is a bow. You currently have " + numArrows + " arrows.";
     }
     public static int medicineEffect(int health, int medicineHeal) {
         return health + medicineHeal;
+    }
+    public static int medicine(int numMedicine) {
+        System.out.println("After the zombie drops dead, a bottle of healing potion appears at your feet.");
+        return numMedicine + 1;
+    }
+    public static double damageCaused(double userAge) {
+        return (userAge - 10) * 2;
+    }
+    public static int numArrows(int numArrows) {
+        return numArrows - 1;
+    }
+    public static int zombieHealth(int zombieHealth, int damageCaused) {
+        return zombieHealth - damageCaused;
+    }
+    public static int health(int health, int damageTaken) {
+        return health - damageTaken;
     }
 }
